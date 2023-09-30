9ab75ec1d20eecf3a213a4b188105b4d13d398ed
Peter Moore <72186925+PeterMoore24@users.noreply.github.com>
2023-09-29 19:15:42 -0700

    Began parsing events & creating event list
    Created defs.py to store class definitions
    Added "Mentors needed" field to form

---
+++ defs.py
@@ -0,0 +1,55 @@
+# I'm going to create an object for each Mentor to keep things organized
+# Source: https://stackoverflow.com/questions/15081542/python-creating-objects
+class Mentor(object):
+	name = ""
+	email = ""
+	# Capacity will store the number of events each mentor said they wanted.
+	capacity = 0
+	# Responses will store how each mentor responded to each event
+	# 1 for yes, 0 for no
+	responses = [0 for i in range(len(event_list))]
+
+	# We need to define a function to initialize each mentor object:
+	def __init__(self, name, email, capacity, responses):
+		self.name = name
+		self.email = email
+		self.capacity = capacity
+		self.responses = responses
+	
+	# This function controls what is printed out when we call print on a mentor object (or str())
+	# Source: https://stackoverflow.com/questions/1535327/how-to-print-instances-of-a-class-using-print
+	def __repr__(self):
+		resp_str = ""
+		for i in range(len(event_list)): 
+			resp_str += str(self.responses[i])
+		return "Mentor: " + str(self.name) + " Email: " + str(self.email) + " Capacity: " + str(self.capacity) + " Responses: " + resp_str
+
+def make_mentor(name, email, capacity, responses):
+	mentor = Mentor(name, email, capacity, responses)
+	return mentor
+
+# I'm going to create an event object to store all of the data related to an event
+class Event(object):
+	name = ""
+	time = ""
+	location = ""
+	need = 0
+	assigned = 0
+	
+	def __init__(self, name, time, location, need, assigned):
+		self.name = name
+		self.time = time
+		self.location = location
+		self.need = need
+		self.assigned = [0 for i in range(need)]
+	
+	def __repr__(self):
+		mentor_string = ""
+		if (len(assigned) > 1):
+			for i in range(need):
+				mentor_string += str(assigned[index]) + " "
+		return "Event: " + str(self.name) + " Time: " + str(self.time) + " Location: " + str(self.location) + " Need: " + str(self.need) + " Assigned: " + mentor_string
+
+def make_event(name, time, location, need, assigned):
+    event = Event(name, time, location, need, assigned)
+    return event
--- scheduling.py
+++ scheduling.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 # This ^ needs to be portable. Maybe a binary would be easier than python? I suspect this program will be run on Windows and MacOS and the less setup the better.
 
+# defs.py defines the event and mentor objects
+import defs
+
 # Step 1: Unzip the results
 # TODO: Get the filename from the command line? Just search for a .zip file in the directory?
 # Source: https://stackoverflow.com/questions/3451111/unzipping-files-in-python
-
-# TODO: THESE LINES MUST BE UNCOMMENTED BEFORE SHIPPING - FOR TESTING
-# import zipfile
-# with zipfile.ZipFile("Mentor Scheduling Form.csv.zip","r") as zip_ref:
-# 	zip_ref.extractall(".")
+import zipfile
+with zipfile.ZipFile("Mentor Scheduling Form.csv.zip","r") as zip_ref:
+	zip_ref.extractall(".")
 
 
 # Step 2: Read resulting csv file
@@ -27,52 +28,37 @@ with open("Mentor Scheduling Form.csv") as csv_file:
 # Step 3: Parse the csv file
 emails = full_dictionary["Username"]
 names = full_dictionary["Please enter your name:"]
-capacities = full_dictionary["How many hours would you like?"]
+capacities = full_dictionary["How many shifts would you like?"]
 
 # event_dictionary contains the list of the events and the responses to each
 event_dictionary = dict(full_dictionary)
 del event_dictionary["Timestamp"]
 del event_dictionary["Username"]
 del event_dictionary["Please enter your name:"]
-del event_dictionary["How many hours would you like?"]
+del event_dictionary["How many shifts would you like?"]
 
-# event_list contains just the list of events
-event_list = list(event_dictionary.keys())
+simple_event_list = list(event_dictionary.keys())
+# event_list should contain a list of all the event objects
+event_list = [0 for i in range(len(simple_event_list))]
+for index in range(len(event_dictionary)):
+	event_name = simple_event_list[index].split("Event: ", 1)[1]
+	event_name = event_name.split("Time: ", 1)[0]
 
-#print(event_dictionary[event_list[1]][1])
+	event_time = simple_event_list[index].split("Time: ", 1)[1]
+	event_time = event_time.split("Location: ", 1)[0]
+
+	event_location = simple_event_list[index].split("Location: ", 1)[1]
+	event_location = event_location.split("Mentors needed: ", 1)[0]
+
+	event_need = int(simple_event_list[index].split("Mentors needed: ", 1)[1])
+
+	blank_assigned = [0 for i in range(event_need)]
+	event_list[index] = make_event(event_name, event_time, event_location, event_need, blank_assigned)
+	print(event_list[index])
+
 
 # Step 3.5: Organize the mentors and their responses.
-# I'm going to create an object for each Mentor to keep things organized
-# Source: https://stackoverflow.com/questions/15081542/python-creating-objects
-class Mentor(object):
-	name = ""
-	email = ""
-	# Capacity will store the number of events each mentor said they wanted.
-	capacity = 0
-	# Responses will store how each mentor responded to each event
-	# 1 for yes, 0 for no
-	responses = [0 for i in range(len(event_list))]
 
-	# We need to define a function to initialize each mentor object:
-	def __init__(self, name, email, capacity, responses):
-		self.name = name
-		self.email = email
-		self.capacity = capacity
-		self.responses = responses
-	
-	# This function controls what is printed out when we call print on a mentor object (or str())
-	# Source: https://stackoverflow.com/questions/1535327/how-to-print-instances-of-a-class-using-print
-	def __repr__(self):
-		resp_str = ""
-		for i in range(len(event_list)): 
-			resp_str += str(self.responses[i])
-		return "Mentor: " + str(self.name) + " Email: " + str(self.email) + " Capacity: " + str(self.capacity) + " Responses: " + resp_str
-
-def make_mentor(name, email, capacity, responses):
-	mentor = Mentor(name, email, capacity, responses)
-	return mentor
-
-print(event_dictionary)
 # Next, we need an array of those mentors so we can iterate through them
 # This mentors should be in the same order in this array as in the form - first come, first assigned
 mentor_list = [0 for i in range(len(names))]
@@ -87,7 +73,7 @@ for mentor_index in range(len(names)):
 	mentor_list[mentor_index] = make_mentor(names[mentor_index], emails[mentor_index], capacities[mentor_index], mentor_response)
 
 # Now, we have an array of all of the mentors' names, emails, capacities, and responses - hooray!
-print(mentor_list)
 
 
 # Step 4: Assign mentors to events
+print(event_list)
